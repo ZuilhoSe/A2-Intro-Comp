3c6158df62f2a2a5d560b33266173e84869d306b
LuisHDBueno <luisbuenopro@gmail.com>
2022-05-26 18:19:11 -0300

    Crie a função que cria as folhas da planilha

--- excel/criar_excel.py
+++ excel/criar_excel.py
@@ -1,4 +1,4 @@
-from openpyxl import Workbook
+from openpyxl import Workbook, load_workbook
 
 #Cria a planilha
 def criar_planilha(nome_da_planilha):
@@ -6,4 +6,17 @@ def criar_planilha(nome_da_planilha):
     nome_arquivo = nome_da_planilha + ".xlsx"
     planilha.save(nome_arquivo)
 
+#Coloca as folhas nomeadas
+def criar_folhas(nome_arquivo):
+    #Abre o arquivo
+    planilha = load_workbook(nome_arquivo)
+    folha1 = planilha.active
+    #Criar
+    folha1.title = "Carteira"
+    folha2 = planilha.create_sheet("Estatísticas", 1)
+    folha3 = planilha.create_sheet("YFINANCE", 2)
+
+    planilha.save(nome_arquivo)
+
 criar_planilha("teste")
+criar_folhas("teste.xlsx")
