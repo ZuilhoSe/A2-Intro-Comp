161ffa9639c40679e994bd072f24088ca6ee4461
Vinícius Sousa <54545608+ViniAS@users.noreply.github.com>
2022-05-26 17:50:32 -0300

    Delete excel directory

--- excel/criar_excel.py
+++
@@ -1,9 +0,0 @@
-from openpyxl import Workbook
-
-#Cria a planilha
-def criar_planilha(nome_da_planilha):
-    planilha = Workbook()
-    nome_arquivo = nome_da_planilha + ".xlsx"
-    planilha.save(nome_arquivo)
-
-criar_planilha("teste")
