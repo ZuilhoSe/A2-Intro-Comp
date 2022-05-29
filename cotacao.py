c7a95abd51240f8de41ef9b7b15c6add06c73c19
BrunoKauanL <brunokauanlunardon@gmail.com>
2022-05-29 16:03:01 -0300

    Atualize a função cotcao_semana para retornar um dicionário

--- cotacao.py
+++ cotacao.py
@@ -7,14 +7,14 @@ def cotacao_semana(dic):
         dic (Dictionary): Espera um dicionário onde as chaves são os códigos de cada ativo
 
     Returns:
-        list: Retorna uma lista onde cada elementos é um dataframe das informações mais relevantes do ativo nos últimos 5 dias 
+        Dictionary: Retorna um dicionário onde cada chave é um ativo e cada valor é um dataframe das informações mais relevantes do ativo nos últimos 5 dias 
     """    
-    lista_semanal = []
-    for x in dic.keys():
-        y=yf.Ticker(x)
-        y_hist = y.history(period="5d")
-        lista_semanal.append(y_hist)
-    return lista_semanal
+    dicionario_semana = {}
+    for ativo in dic.keys():
+        ticket=yf.Ticker(ativo)
+        ticket_hist = ticket.history(period="5d")
+        dicionario_semana[ativo] = ticket_hist
+    return dicionario_semana
 
 def cotacao_anual(dic):
     """A função recebe um dicionario que contém o nome das açoes como chaves e retorna a cotação do ativo no último ano
