de531e1da934fb3a4e971131fde8dd17d26779a4
BrunoKauanL <brunokauanlunardon@gmail.com>
2022-05-29 15:13:38 -0300

    Remova as linhas de teste e crie as docstrings

--- cotacao.py
+++ cotacao.py
@@ -1,8 +1,14 @@
 import yfinance as yf
 
-dic = {"MGLU3.SA":"1000", "PETR4.SA":"100000"}
-#A função recebe um dicionario que contém o nome das açoes como chaves e retorna a cotação do ativo na última semana útil
 def cotacao_semana(dic):
+    """A função recebe um dicionario que contém o nome das açoes como chaves e retorna a cotação do ativo na última semana útil
+
+    Args:
+        dic (Dictionary): Espera um dicionário onde as chaves são os códigos de cada ativo
+
+    Returns:
+        list: Retorna uma lista onde cada elementos é um dataframe das informações mais relevantes do ativo nos últimos 5 dias 
+    """    
     lista_semanal = []
     for x in dic.keys():
         y=yf.Ticker(x)
@@ -10,15 +16,18 @@ def cotacao_semana(dic):
         lista_semanal.append(y_hist)
     return lista_semanal
 
-print(cotacao_semana(dic)[0])
-
-#A função recebe um dicionario que contém o nome das açoes como chaves e retorna a cotação do ativo no último ano
 def cotacao_anual(dic):
+    """A função recebe um dicionario que contém o nome das açoes como chaves e retorna a cotação do ativo no último ano
+
+    Args:
+        dic (Dictionary): Espera um dicionário onde as chaves são os códigos de cada ativo
+
+    Returns:
+        list: Retorna uma lista onde cada elementos é um dataframe das informações mais relevantes do ativo no último ano
+    """  
     lista_anual = []
     for x in dic.keys():
         y=yf.Ticker(x)
         y_hist = y.history(period="1y")
         lista_anual.append(y_hist)
     return lista_anual
-
-print(cotacao_anual(dic)[0])
