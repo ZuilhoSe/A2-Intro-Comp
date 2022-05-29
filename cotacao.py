38fa02204e0ed9320b506dd1a51fb7284155ac63
BrunoKauanL <brunokauanlunardon@gmail.com>
2022-05-29 14:38:27 -0300

    Crie a função cotacao_anual

--- cotacao.py
+++ cotacao.py
@@ -11,3 +11,14 @@ def cotacao_semana(dic):
     return lista_semanal
 
 print(cotacao_semana(dic)[0])
+
+#A função recebe um dicionario que contém o nome das açoes como chaves e retorna a cotação do ativo no último ano
+def cotacao_anual(dic):
+    lista_anual = []
+    for x in dic.keys():
+        y=yf.Ticker(x)
+        y_hist = y.history(period="1y")
+        lista_anual.append(y_hist)
+    return lista_anual
+
+print(cotacao_anual(dic)[0])
