d5e9f2159b6ae3a6cd3bc81c1a664570f9ab9002
LuisHDBueno <luisbuenopro@gmail.com>
2022-05-28 21:58:08 -0300

    Crie a função link_folhas

--- criar_excel.py
+++ criar_excel.py
@@ -16,10 +16,27 @@ def criar_folhas(nome_arquivo):
     #Salva a planilha
     planilha.save(nome_arquivo)
 
+#Cria o Link entre as duas folhas
+def link_folhas(nome_arquivo):
+    #Abre o arquivo
+    planilha = load_workbook(nome_arquivo)
+    #Seleciona as folhas
+    carteira = planilha["Carteira"]
+    estatisticas = planilha["Estatísticas"]
+    #Cria as strings dos links
+    link_para_estatisticas = f"=HYPERLINK(\"[{nome_arquivo}]Estatísticas!A1\",\"Estatísticas\")"
+    link_para_carteira = f"=HYPERLINK(\"[{nome_arquivo}]Carteira!A1\",\"Carteira\")"
+    #Adiciona o link a celula A1 de cada folha
+    carteira["A1"] = link_para_estatisticas
+    estatisticas["A1"] = link_para_carteira
+    #Salva a planilha
+    planilha.save(nome_arquivo)
+
 #Cria o a planilha base
 def criar_planilha(nome_carteira):
     nome_arquivo = nome_carteira +".xlsx"
     criar_arquivo(nome_arquivo)
     criar_folhas(nome_arquivo)
+    link_folhas(nome_arquivo)
 
 criar_planilha("Teste")
