69d5456d913334e96c3d5865f8ac08058e5d8391
BrunoKauanL <brunokauanlunardon@gmail.com>
2022-05-30 02:35:54 -0300

    Renomeie a função fator_conversao para buscar_fatores e atualize a função conversao

--- cotacao.py
+++ cotacao.py
@@ -1,9 +1,39 @@
 import yfinance as yf
 import time
 
-dic = {"PETR4.SA":"10", "AMZN":"100"}
-
+dic = {"AMZN":"100"}
 fator_conversao = 2
+
+
+def buscar_fator(dic):
+    dicionario_moeda = {}
+    dicionario_fatores = {}
+    for ativo in dic.keys():
+        ticket=yf.Ticker(ativo)
+        ticket_currency = ticket.info["currency"]
+        if ticket_currency != "BRL":
+            dicionario_moeda[ativo] = ticket_currency+"BRL=X"
+        else:
+            dicionario_moeda[ativo] = ticket_currency 
+        for value in dicionario_moeda.values():
+            if value != "BRL":
+                fator = yf.Ticker(value)
+                fator_info = fator.info["regularMarketPrice"]
+                dicionario_fatores[ativo] = fator_info
+            else:
+                dicionario_fatores[ativo] = 1
+    return dicionario_fatores
+
+
+
+def conversao(ticket_hist, dic):
+    fatores = buscar_fator(dic)
+    for ativo in dic.keys():
+        fator_conversao = fatores[ativo]
+        df = ticket_hist  
+        df["Close"] = fator_conversao * df["Close"]
+        df["Open"] = fator_conversao * df["Open"]
+    return ticket_hist
 
 def cotacao_semana(dic):
     """A função recebe um dicionario que contém o nome das açoes como chaves e retorna a cotação do ativo na última semana útil
@@ -17,10 +47,11 @@ def cotacao_semana(dic):
         ticket=yf.Ticker(ativo)
         ticket_hist = ticket.history(period="5d")
         #chama a funçao conversão para corrigir o valor das colunas necessarias para BRL quando o ativo estiver cotado em outra moeda
-        ticket_hist = conversao(ticket_hist)
+        ticket_hist = conversao(ticket_hist, dic)
         dicionario_semana[ativo] = ticket_hist
     return dicionario_semana
 
+print(cotacao_semana(dic))
 
 def cotacao_anual(dic):
 
@@ -35,83 +66,10 @@ def cotacao_anual(dic):
         ticket=yf.Ticker(ativo)
         ticket_hist = ticket.history(period="1y")
         #chama a funçao conversão para corrigir o valor das colunas necessarias para BRL quando o ativo estiver cotado em outra moeda
-        ticket_hist = conversao(ticket_hist)
+        ticket_hist = conversao(ticket_hist, dic)
         dicionario_anual[ativo] = ticket_hist
     return dicionario_anual
 
-def conversao(ticket_hist):
-    df = ticket_hist  
-    df["Close"] = fator_conversao * df["Close"]
-    df["Open"] = fator_conversao * df["Open"]
-    return ticket_hist
 
 
 
-def fator_conversao(dic):
-    dicionario_moeda = {}
-    for ativo in dic.keys():
-        ticket=yf.Ticker(ativo)
-        ticket_currency = ticket.info["currency"]
-        if ticket_currency != "BRL":
-            dicionario_moeda[ativo] = ticket_currency+"BRL=X"
-        else:
-            dicionario_moeda[ativo] = ticket_currency 
-    for value in dicionario_moeda.values():
-        fator = yf.Ticker(value)
-        fator_info = fator.info["regularMarketPrice"] 
-    return dicionario_moeda, fator_info
-
-
-print(fator_conversao(dic))
-
-
-
-
-
-
-"""
-tickets=str()
-for ativo in dic.keys():
-    tickets = tickets+ativo+" "
-
-ticket=yf.download(tickets, group_by="ticker", period="5d", progress=False)
-for ativo in dic.keys():
-    print(ticket[ativo].drop("Adj Close",axis=1).reset_index())
-"""
-
-
-#def conversão(f, dic):
-
-
-
-#print(conversão(cotacao_semana(dic), dic))
-
-
-#print(cotacao_semana(dic))
-
-
-#yahooo query - info
-#yf.download
-#Tickers = list(dic.keys())
-#a = "AMZN"
-#df = yf.download(a, period="1mo", interval="1wk", progress=False)
-#print(df)
-#função dowload retorna dataframes diferentes pra 1 ativo ou pra mais ativos
-#print(df.columns)
-
-#acesso certo dataframe df=["close", "PETR4.SA"]
-
-
-
-#currency = {"AMZN": "USD"}
-#fator = currency.values()
-
-#df["aa"] = fator[]"""
-
-
-"""
-tempo_inicial = time.time()
-tempo_final = time.time()
-print(f"{tempo_final - tempo_inicial} segundos")
-calculo de tempo de execução
-"""
