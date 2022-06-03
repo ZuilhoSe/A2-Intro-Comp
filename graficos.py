9b7cd0fcccc313607fb870770397ba30b2214c12
George Dutra <georgedutra661@gmail.com>
2022-06-02 22:57:44 -0300

    Plote o gráfico com sucesso, sem estilizar

--- graficos.py
+++ graficos.py
@@ -4,16 +4,14 @@ from openpyxl.chart.marker import DataPoint
 from datetime import datetime, timedelta
 import yfinance as yf
 import locale
-locale.setlocale(locale.LC_ALL, ("pt_BR", "utf-8"))
 import pandas
 import calendar
 
-# Dicionário de teste:
-carteira = {"PETR4.SA":"10", "AMZN":"10", "AAPL":"100", "KO":"100"}
+locale.setlocale(locale.LC_ALL, ("pt_BR", "utf-8"))
 
-def graf_barras(base, planilha):
+def graf_barras(base, arquivo):
     
-    #O gráfico de barras mostra quanto uma ação subiu ou desceu de valor no último ano, e para isso devemos primeiro adquirir os dados pelo dia_hoje finance, e em seguida montar o gráfico
+    #O gráfico de barras mostra quanto uma ação subiu ou desceu de valor no último ano, e para isso devemos primeiro adquirir os dados pelo yfinance, e em seguida montar o gráfico
 
     #Primeiro, o código para pegar a data de um ano atrás:
     dias = 365
@@ -37,7 +35,7 @@ def graf_barras(base, planilha):
         ticket = yf.Ticker(ativo)
         ticket_hist = ticket.history(start=data_inicio, end=data_final)
 
-        #É muito comum que em determinado dia, a bolsa não funcione por vários motivos, e por isso o Yfinance não terá dados referentes àquele dia específico, e nos entregará um dataframe vazio, por isso devemos tratar este caso. Decidi tratar de tal forma que, caso o dataframe retornado seja vazio, iremos pegar os dados de um dia antes, repetidamente, até ser retornado um dataframe válido
+        #É muito comum que em determinado dia, a bolsa não funcione por vários motivos, e por isso o Yfinance não terá dados referentes àquele dia específico, e nos entregará um dataframe vazio, por isso devemos tratar este caso. Caso o dataframe retornado seja vazio, iremos pegar os dados de um dia antes, repetidamente, até ser retornado um dataframe válido
         while ticket_hist.size == 0:
             dias += 1
             inicio_altern = datetime.now() - timedelta(days=dias)
@@ -63,35 +61,55 @@ def graf_barras(base, planilha):
 
     print(valores_hoje)
     
-    """
-    #Carregar a planilha e selecionar a folha de trabalho
-    planilha = load_workbook(planilha)
-    folha = planilha.active
+    #Agora, vamos fazer a matemágica para chegar nos dados necessários
+    dados = []
+    for chave in valores_antigos.keys():
+        x = valores_hoje[chave] / valores_antigos[chave]
+        resultado = x - 1
+        dados.append(resultado)
+    print(dados)
+
+    #E as categorias sendo as chaves:
+    categorias = []
+    for chave in valores_antigos.keys():
+        categorias.append(chave)
+
+    #Agora que temos os dados, entramos na etapa de montar o gráfico:
+
+    #Carregamos a planilha e selecionamos a folha de trabalho
+    planilha = load_workbook(arquivo)
+    estatisticas = planilha["Estatísticas"]
     
+    #Inserimos os dados na planilha
+    linha_d = 2
+    for dado in dados:
+        linha_d += 1
+        estatisticas[f"C{linha_d}"].value = dado        
     
-    #Seleciona o tipo e os titulos do grafico
+    linha_c = 2
+    for cat in categorias:
+        linha_c += 1
+        estatisticas[f"D{linha_c}"].value = cat
+
+    #Selecionamos o tipo e os títulos do grafico
     grafic = BarChart()
     grafic.title = "Variação das Ações no Último Ano"
     grafic.type = "col"
-    grafic.style = 7
+    grafic.style = 9
     grafic.y_axis.title = "Variação"
-    grafic.x_axis.title = "Ativo"
+    grafic.x_axis.title = "Ativos"
     grafic.x_axis.tickLblPos = "low"
 
     #Seleciona os dados do grafico
-    dados = Reference(folha, min_col=3, min_row=2, max_col=3, max_row=14)
-    nomes = Reference(folha, min_col=2, min_row=3, max_col=2, max_row=14)
-    grafic.add_data(dados, titles_from_data=True)
-    grafic.set_categories(nomes)
+    dados_finais = Reference(estatisticas, min_col=3, min_row=3, max_col=3, max_row=f"{linha_d}")
+    categorias_finais = Reference(estatisticas, min_col=4, min_row=3, max_col=4, max_row=f"{linha_c}")
+    grafic.add_data(dados_finais, titles_from_data=False)
+    grafic.set_categories(categorias_finais)
 
     #Modificando as cores das barras que representam valores_antigos menores que zero
-    rows = []
-    for row in folha.iter_rows(min_row=3, min_col=2, max_col=3, max_row=14, values_only=True):
-        rows.append(row)
-
     series = grafic.series[0]
-    for i in range(len(rows)):
-        if rows[i][1] < 0:
+    for i in range(len(dados)):
+        if dados[i] < 0:
             pt = DataPoint(idx=i)
             pt.graphicalProperties.line.solidFill = 'DC143C'
             pt.graphicalProperties.solidFill = 'DC143C'
@@ -99,15 +117,14 @@ def graf_barras(base, planilha):
 
     #Estilizando as séries
     grafic.legend = None
-    grafic.shape = 1
 
     #Adiciona o gráfico à planilha
-    folha.add_chart(grafic, "B20")
+    estatisticas.add_chart(grafic, "F3")
+    planilha.save(arquivo)
 
-    planilha.save("teste 2.xlsx")
-
-    """
-    
 """==========================================================================================="""
 
-graf_barras(carteira, "aaa")
+# Dicionário de teste:
+carteira = {"PETR4.SA":"10", "AMZN":"10", "AAPL":"100", "KO":"100"}
+
+graf_barras(carteira, "teste.xlsx")
