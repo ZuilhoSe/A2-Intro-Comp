b37f40032311c2d4d46fe6553324718f48f76530
Zuilho Segundo <segundozuilho@gmail.com>
2022-06-17 13:12:55 -0300

    Criacao de graficos
    Documentação e implementação das funções criadoras de gráficos.

--- app.py
+++ app.py
@@ -4,10 +4,17 @@ import scrapper
 import cotacao
 import criar_excel
 import carteira
-
+import graficos
 
 class Application:
+    """Classe necessária para utilização do Tkinter
+    """    
     def __init__(self, master=None):
+        """Inicialização da classe da aplicação
+
+        Args:
+        Todos os atributos são definidos e atualizados durante a inicialização.
+        """        
         self.valida = False
 
         self.fontePadrao = ("Arial", "10")
@@ -60,44 +67,69 @@ class Application:
 
     #Método verificar url
     def verificaURL(self):
+        """Método para verificar a URL e realizar o processo de busca na carteira.
+        """    
         self.url_coletada = self.url.get()
         if validators.url(self.url_coletada) == True:
             self.realizar_scrapper()
             self.buscar_cotacoes()
             self.criando_excel()
             self.criando_carteira()
+            self.criando_graficos()
+            self.mensagem["text"] = "Programa Finalizado! Pode abrir o arquivo .xlsx"
         else:
             self.mensagem["text"] = "URL INVÁLIDA! Certifique-se de que a URL está correta!"
 
     #Método para realização do scrapper;
     def realizar_scrapper(self):
+        """Método que realiza as funções do scrapper.py
+        """        
         soup = scrapper.ler_carteira(self.url_coletada)
         acoes = scrapper.buscar_acoes(soup)
         moedas = scrapper.buscar_moedas(soup)
         lista = scrapper.juntar_moedas_acoes(acoes, moedas)
         self.carteira = scrapper.saida(lista)
+        self.mensagem["text"] = "Scrapper Realizado"
 
     #Método que executa o módulo de buasca de cotações;
     def buscar_cotacoes(self):
+        """Método que executa as funções do cotacoes.py
+        """        
         self.cotacao_anual = cotacao.cotacao_anual(self.carteira)
         self.cotacao_semanal = cotacao.cotacao_semanal(self.carteira)
         self.cotacao_atual = cotacao.cotacao_atual(self.carteira)
+        self.mensagem["text"] = "Cotações Buscadas"
 
     #Método que cria as planilhas excel;
     def criando_excel(self):
+        """Método que executa as funções do criar_excel.py
+        """        
         self.xl_nome = self.xl.get()
         if ".xlsx" in self.xl_nome:
             criar_excel.criar_planilha(self.xl_nome)
         else:
             criar_excel.criar_planilha(self.xl_nome + ".xlsx")
+        self.mensagem["text"] = "Planilha Criada"
 
-    #método que cria os gráficos;
+    #Método que cria a carteira;
     def criando_carteira(self):
+        """Método que executa as funções da carteira.py
+        """        
         if ".xlsx" in self.xl_nome:
-            nome_xl = self.xl_nome
+            self.nome_xl = self.xl_nome
         else:
-            nome_xl = self.xl_nome + ".xlsx"
-        carteira.carteira(nome_xl, self.carteira)
+            self.nome_xl = self.xl_nome + ".xlsx"
+        carteira.carteira(self.nome_xl, self.carteira)
+        self.mensagem["text"] = "Carteira Criada"
+
+    #Método que cria os gráficos;
+    def criando_graficos(self):
+        """Método que executa as funções do grafico.py
+        """        
+        graficos.graf_barras(self.carteira, self.nome_xl)
+        graficos.graf_linha(self.carteira, self.cotacao_anual, self.nome_xl)
+        graficos.graf_stock(self.cotacao_anual, self.nome_xl)
+        self.mensagem["text"] = "Gráficos Criados"
 
 #Execução do Aplciativo
 root = Tk()
