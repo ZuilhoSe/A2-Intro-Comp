bad2b9b183fdebb0c3d909dfc6332e12c8fe8b08
LuisHDBueno <luisbuenopro@gmail.com>
2022-06-18 12:07:41 -0300

    Mude os parametros de proteção das folhas da planilha

--- criar_excel.py
+++ criar_excel.py
@@ -73,7 +73,7 @@ def bloquear_planilha(nome_arquivo):
 
     Args:
         nome_arquivo (string): deve estar no formato "nome_arquivo.xlsx"
-    """    
+    """
     #Abre o arquivo
     planilha = load_workbook(nome_arquivo)
     #Seleciona as folhas
@@ -85,10 +85,19 @@ def bloquear_planilha(nome_arquivo):
     #Remover headers
     carteira.sheet_view.showRowColHeaders = False
     estatisticas.sheet_view.showRowColHeaders = False
-    #Proteger planilha
+    #Proteger as folhas
+    carteira.protection.selectLockedCells = True
+    carteira.protection.selectUnlockedCells = True
+    carteira.protection.objects = True
+    carteira.protection.scenarios = True
     carteira.protection.password = "protegida"
+    estatisticas.protection.selectLockedCells = True
+    estatisticas.protection.selectUnlockedCells = True
+    estatisticas.protection.objects = True
+    estatisticas.protection.scenarios = True
     estatisticas.protection.password = "protegida"
-    planilha.security = WorkbookProtection(workBookPassword = "protegida", lockStructure = True)
+    #Proteger o planilha
+    planilha.security = WorkbookProtection(workbookPassword = "protegida", lockStructure = True)
     #Salva a planilha
     planilha.save(nome_arquivo)
 
